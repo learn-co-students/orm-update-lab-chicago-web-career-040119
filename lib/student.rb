d234cf64f117506f057e585402ddf7b80e650146
diautzi <dia_utzi@yahoo.com>
2019-04-10 21:39:03 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -72,8 +72,8 @@ class Student
       end
 
     def update
-      sql = "UPDATE students SET name = ?, grade = ?, WHERE id =?"
-      DN[:conn].execute(sql, self.name, self.grade, self.id)
+      sql = "UPDATE students SET name = ?, grade = ? WHERE id =?"
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
 end
